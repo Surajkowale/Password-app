ec94b6e2fe74cb9716f5236a328198146c4b9e64
Surajkowale <133976177+Surajkowale@users.noreply.github.com>
2023-06-05 17:24:15 +0530

    Add files via upload

---
+++ PasswordManagerApplication.java
@@ -0,0 +1,85 @@
+package de.app.passwordmanager;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.color.DynamicColors;
+
+import de.app.passwordmanager.database.SecureElementDatabase;
+import de.app.passwordmanager.ui.login.LoginActivity;
+import de.app.passwordmanager.utils.PreferenceUtil;
+import de.app.passwordmanager.utils.TimeoutUtil;
+
+public class PasswordManagerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            final TimeoutUtil timeoutUtil = new TimeoutUtil();
+            Activity lastPaused;
+
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    activity.getWindow().getDecorView().setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS);
+                }
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {}
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                if(lastPaused == null){
+                    timeoutUtil.initiateDelay();
+                    return;
+                }
+
+                // If activities are the same, the app was paused and a re-authentication check is
+                // performed
+                if(lastPaused != activity)
+                    return;
+
+                timeoutUtil.initiateDelay();
+                long time = PreferenceUtil.getTimeForNewAuthentication(activity);
+                if(time < 0)
+                    return;
+
+                if(time == Long.MAX_VALUE || timeoutUtil.delayMet(time * 60000)) {
+                    activity.startActivity(LoginActivity.getIntentForAuthentication(activity, activity.getIntent()));
+                    activity.finish();
+                }
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+                lastPaused = activity;
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {}
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {}
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {}
+        });
+    }
+
+    public SecureElementDatabase getDatabase(){
+        return SecureElementDatabase.createAndGet(this);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Password-app
-i write a code for app
+# Password Manager
+![repository-logo](https://user-images.githubusercontent.com/42292083/219877763-f1f6b699-5f22-465d-aaf4-6f81c970ef94.png)
+
+A secure, open-source Android password manager that stores passwords and credit card information encrypted on a local device
+
+## Get a first impression
+<div align="center"/>
+<img src="https://user-images.githubusercontent.com/42292083/232009269-ad9e913c-55eb-48a7-986e-daa1b7e53ebd.png" alt="Dashboardside" width="300"/>
+<img src="https://user-images.githubusercontent.com/42292083/232009273-d393a1a8-8698-481b-956d-9c454a003c9a.png" alt="Viewingside" width="300"/>
+</div>
+
+## Features
++ Store Passwords and Credit Card information
++ Generate secure Passwords and Passphrases
++ Estimate the Password Strength with [nbvcxz](https://github.com/GoSimpleLLC/nbvcxz)
++ AES-Encryption on your local device
++ Autofill feature for easy input
++ Supports Material 3 and Dynamic Color (Android 12+ required)
+
+## Available On
+|Platform|Status|
+|--------|------|
+|GitHub|[![GitHub tag](https://img.shields.io/github/release/OffRange/PasswordManager?include_prereleases=&sort=semver)](https://github.com/OffRange/PasswordManager/releases/)|
+|Google Play Store|Planned for July/August 2023|
+
+## Installation Guide
+1. Download the latest [APK file](https://github.com/OffRange/PasswordManager/releases/latest) under "Assets"
+2. Open the **"Downloads"** folder on your device and tap the APK file to begin the installation process.
+4. A pop-up message will appear asking for your permission to install the app. Tap **"Settings"** on the pop-up message.
+5. On the settings page, toggle the switch next to **"Allow from this source"** to grant permission to install apps from GitHub releases.
+6. Go back to the installation screen and **tap "Install"** to continue with the installation process.
+7. Installation completed
